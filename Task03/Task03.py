0991014af5b89f058904beab70029c1587b2d00b
Pavel Arvachev <arvachev@gmail.com>
2022-12-08 15:28:12 +0300

    Add task03

--- Task03/Task03.py
+++ Task03/Task03.py
@@ -1,1 +1,13 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+import random
+
+
+a = []
+b = []
+for i in range(random.randint(1, 10)):
+    t = float('.' + str(random.randint(1, 30)))
+    a.append(float(str(random.randint(1, 10)) + str(t)))
+    b.append(t)
+
+print(a)
+print(round(max(b) - min(b), 3))
